90a1298ae1211c10bfedf0f5beaf6d8eaf55474b
JP Fielding <jp.fielding@gmail.com>
2025-11-22 18:39:33 +0000

    go-adk: quick start

---
+++ go-adk/quick_start/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"google.golang.org/adk/agent"
+	"google.golang.org/adk/agent/llmagent"
+	"google.golang.org/adk/cmd/launcher"
+	"google.golang.org/adk/cmd/launcher/full"
+	"google.golang.org/adk/model/gemini"
+	"google.golang.org/adk/tool"
+	"google.golang.org/adk/tool/geminitool"
+	"google.golang.org/genai"
+)
+
+func main() {
+	ctx, cnc := context.WithCancel(context.Background())
+	defer cnc()
+
+	model, err := gemini.NewModel(ctx, "gemini-3-pro-preview", &genai.ClientConfig{
+		APIKey: os.Getenv("GOOGLE_API_KEY"),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create model: %v", err)
+	}
+
+	timeAgent, err := llmagent.New(llmagent.Config{
+		Model:       model,
+		Name:        "quick_start_agent",
+		Description: "Tells the current time in a specified city.",
+		Tools: []tool.Tool{
+			geminitool.GoogleSearch{},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Failed to create agent: %v", err)
+	}
+	config := &launcher.Config{
+		AgentLoader: agent.NewSingleLoader(timeAgent),
+	}
+
+	l := full.NewLauncher()
+	if err := l.Execute(ctx, config, os.Args[1:]); err != nil {
+		log.Fatalf("Failed to execute launcher: %v", err)
+	}
+}
